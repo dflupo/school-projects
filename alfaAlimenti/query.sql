7c5b2ad06b1a472efa5f685496252d54be13fb34
Domenico Lupo <domenicolupo.ce@gmail.com>
2022-03-11 09:49:27 +0100

    Alfa ALimenti fix

--- alfaAlimenti/query.sql
+++ alfaAlimenti/query.sql
@@ -1,5 +1,31 @@
-
-SELECT Categorie.IDCategoria, AVG(Prodotti.prezzoUnitario) AS "Media Prezzi"
-FROM Categorie, Prodotti
-WHERE Categorie.IDCategorie = Prodotti.IDCategoria
-GROUP BY Categoie.IDCategoria
+-– numero 1 —
+select Categorie.IDCategoria, avg(Prodotti.PrezzoUnitario) as “Media prezzi”
+from Categorie, Prodotti
+where Categorie.IDCategoria = Prodotti.IDCategoria
+group by Categorie.IDCategoria
+— numero 2 —
+select Categorie.NomeCategoria, min(Prodotti.PrezzoUnitario) as “Prezzo minore”,
+max(Prodotti.PrezzoUnitario) as “Prezzo maggiore.”
+from Categorie, Prodotti
+where Categorie.IDCategoria = Prodotti.IDCategoria
+group by Categorie.NomeCategoria
+— numero 3 —
+select Fornitori.IDFornitore, count(Prodotti.IDProdotto) as “Numero prodotti forniti”
+from Fornitori, Prodotti
+where Fornitori.IDFornitore = Prodotti.IDFornitore
+group by Fornitori.IDFornitore
+— numero 4 —
+select Fornitori.NomeSocieta, count(Prodotti.IDProdotto) as “Numero prodotti forniti”
+from Fornitori, Prodotti
+where Fornitori.IDFornitore = Prodotti.IDFornitore
+group by Fornitori.NomeSocieta
+— numero 5 —
+select Corrieri.IDCorriere, avg(Ordini.Trasporto) as “Media spese di trasporto”
+from Corrieri, Ordini
+where Corrieri.IDCorriere = Ordini.IDOrdine
+group by Corrieri.IDCorriere
+— numero 6 —
+select Corrieri.NomeSocieta, avg(Ordini.Trasporto) as “Media spese di trasporto”
+from Corrieri, Ordini
+where Corrieri.IDCorriere = Ordini.IDOrdine
+group by Corrieri.NomeSocieta
